10de0114729655043399efc80cda4e8f70b4383a
DEVANSH BANSAL <devanshbansal321@gmail.com>
2025-10-30 20:11:03 +0530

    .

---
+++ .vscode/temporary file.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+using namespace std;
+{
+    int main()
+    {
+        int x,y;
+        x=5;
+        y=x%2;
+        if(y==0)
+        {
+            cout<<"no. is even";
+        }
+        else
+        cout<<"no.is oddd";
+
+    }
+}
